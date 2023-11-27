a91809c2e359afe6ddb25926264c1a2243938dfe
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2023-11-27 11:45:55 +0100

    Update FullTSLinearSeries.js

--- app/engine/utils/FullTSLinearSeries.js
+++ app/engine/utils/FullTSLinearSeries.js
@@ -4,7 +4,7 @@
 
   The TSLinearSeries is a datatype that represents a Linear Series. It allows
   values to be retrieved (like a FiFo buffer, or Queue) but it also includes
-  a full Theil-Sen Linear Regression estimator to determine the slope of this timeseries.
+  a TheilÃ¢â‚¬â€œSen estimator Linear Regressor to determine the slope of this timeseries.
 
   At creation its length is determined. After it is filled, the oldest will be pushed
   out of the queue) automatically.
@@ -23,13 +23,13 @@ import { createSeries } from './Series.js'
 import loglevel from 'loglevel'
 const log = loglevel.getLogger('RowingEngine')
 
-function createFullTSLinearSeries (maxSeriesLength = 0) {
+function createTSLinearSeries (maxSeriesLength = 0) {
   const X = createSeries(maxSeriesLength)
   const Y = createSeries(maxSeriesLength)
   const slopes = []
 
-  let _slope = 0
-  let _intercept = 0
+  let _A = 0
+  let _B = 0
   let _goodnessOfFit = 0
 
   function push (x, y) {
@@ -62,18 +62,29 @@ function createFullTSLinearSeries (maxSeriesLength = 0) {
 
     // Calculate the median of the slopes
     if (X.length() > 1) {
-      _slope = median()
+      _A = median()
     } else {
-      _slope = 0
+      _A = 0
     }
+    _B = Y.average() - (_A * X.average())
   }
 
   function slope () {
-    return _slope
+    return _A
   }
 
   function intercept () {
-    return _intercept
+    return _B
+  }
+
+  function coefficientA () {
+    // For testing purposses only!
+    return _A
+  }
+
+  function coefficientB () {
+    // For testing purposses only!
+    return _B
   }
 
   function length () {
@@ -91,15 +102,15 @@ function createFullTSLinearSeries (maxSeriesLength = 0) {
 
   function projectX (x) {
     if (X.length() >= 2) {
-      return (_slope * x) + _intercept
+      return (_A * x) + _B
     } else {
       return 0
     }
   }
 
   function projectY (y) {
-    if (X.length() >= 2 && _slope !== 0) {
-      return ((y - _intercept) / _slope)
+    if (X.length() >= 2 && _A !== 0) {
+      return ((y - _B) / _A)
     } else {
       return 0
     }
@@ -172,7 +183,7 @@ function createFullTSLinearSeries (maxSeriesLength = 0) {
       const mid = Math.floor(sortedArray.length / 2)
       return (sortedArray.length % 2 !== 0 ? sortedArray[mid] : ((sortedArray[mid - 1] + sortedArray[mid]) / 2))
     } else {
-      log.eror('TS Linear Regressor, Median calculation on empty dataset attempted!')
+      log.error('TS Linear Regressor, Median calculation on empty dataset attempted!')
       return 0
     }
   }
@@ -181,8 +192,8 @@ function createFullTSLinearSeries (maxSeriesLength = 0) {
     X.reset()
     Y.reset()
     slopes.splice(0, slopes.length)
-    _slope = 0
-    _intercept = 0
+    _A = 0
+    _B = 0
     _goodnessOfFit = 0
   }
 
@@ -190,6 +201,8 @@ function createFullTSLinearSeries (maxSeriesLength = 0) {
     push,
     slope,
     intercept,
+    coefficientA,
+    coefficientB,
     length,
     goodnessOfFit,
     projectX,
@@ -210,4 +223,4 @@ function createFullTSLinearSeries (maxSeriesLength = 0) {
   }
 }
 
-export { createFullTSLinearSeries }
+export { createTSLinearSeries }
