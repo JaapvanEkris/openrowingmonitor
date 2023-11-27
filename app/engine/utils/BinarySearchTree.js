bc7c6336628505486445a32c230f7bb8d562e453
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2023-11-27 16:25:48 +0100

    Update BinarySearchTree.js

--- app/engine/utils/BinarySearchTree.js
+++ app/engine/utils/BinarySearchTree.js
@@ -1,39 +1,33 @@
-class Node {
-  constructor(val) {
-    this.val = val
-    this.right = null
-    this.left = null
-    this.count = 0
-  }
+// Node class
+class Node
+{
+	constructor(data)
+	{
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
 }
 
-class BST {
-  constructor() {
-    this.root = null
-  }
-  create(val) {
-    const newNode = new Node(val)
-    if (!this.root) {
-      this.root = newNode
-      return this
+// Binary Search tree class
+class BinarySearchTree
+{
+    constructor()
+    {
+        // root of a binary search tree
+        this.root = null;
     }
-    let current = this.root
-
-    const addSide = side => {
-      if (!current[side]) {
-        current[side] = newNode
-        return this
-      }
-      current = current[side]
-    }
-
-    while (true) {
-      if (val === current.val) {
-        current.count++
-        return this
-      }
-      if (val < current.val) addSide('left')
-      else addSide('right')
-    }
-  }
+ 
+    // function to be implemented
+    // insert(data)
+    // remove(data)
+                 
+ 
+    // Helper function
+    // findMinNode()
+    // getRootNode()
+    // inorder(node)
+    // preorder(node)               
+    // postorder(node)
+    // search(node, data)
 }
