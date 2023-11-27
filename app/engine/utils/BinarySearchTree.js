ec5f58ec4e000b401997eeae9414824fb4e215db
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2023-11-27 16:34:18 +0100

    Update BinarySearchTree.js

--- app/engine/utils/BinarySearchTree.js
+++ app/engine/utils/BinarySearchTree.js
@@ -2,9 +2,9 @@
 
 class Node {
   constructor(key) {
-    this.key = key;
-    this.left = null;
-    this.right = null;
+    this.key = key
+    this.left = null
+    this.right = null
   }
 }
 
@@ -13,14 +13,14 @@ class Node {
 function insert(node, key) {
   // If the tree is empty, return a new node
   if (node === null) {
-	  return new Node(key);
+	  return new Node(key)
   }
 
   // Otherwise, recur down the tree
   if (key < node.key) {
-	  node.left = insert(node.left, key);
+	  node.left = insert(node.left, key)
   } else if (key > node.key) {
-	  node.right = insert(node.right, key);
+	  node.right = insert(node.right, key)
   }
 
   // Return the (unchanged) node pointer
@@ -31,43 +31,43 @@ function insert(node, key) {
   function search(root, key) {
   // Base Cases: root is null or key is present at root
   if (root === null || root.key === key) {
-	  return root;
+    return root
   }
 
   // Key is greater than root's key
   if (root.key < key) {
-	  return search(root.right, key);
+	  return search(root.right, key)
   }
 
   // Key is smaller than root's key
-  return search(root.left, key);
+  return search(root.left, key)
   }
 
 // Driver Code
-let root = null;
-root = insert(root, 50);
-insert(root, 30);
-insert(root, 20);
-insert(root, 40);
-insert(root, 70);
-insert(root, 60);
-insert(root, 80);
+let root = null
+root = insert(root, 50)
+insert(root, 30)
+insert(root, 20)
+insert(root, 40)
+insert(root, 70)
+insert(root, 60)
+insert(root, 80)
 
 // Key to be found
-  let key = 6;
+  let key = 6
 
 // Searching in a BST
 if (search(root, key) === null) {
-  console.log(key + " not found");
+  console.log(key + " not found")
 } else {
-  console.log(key + " found");
+  console.log(key + " found")
 }
 
-key = 60;
+key = 60
 
 // Searching in a BST
 if (search(root, key) === null) {
-  console.log(key + " not found");
+  console.log(key + " not found")
 } else {
-  console.log(key + " found");
+  console.log(key + " found")
 }
