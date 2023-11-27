fe665488cd9eec70b1e0c383a6ccee305b5d9fc7
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2023-11-27 14:37:52 +0100

    Update FullTSQuadraticSeries.test.js

--- app/engine/utils/FullTSQuadraticSeries.test.js
+++ app/engine/utils/FullTSQuadraticSeries.test.js
@@ -316,7 +316,7 @@ test('Quadratic Approximation on function y = 4 * Math.pow(x, 2) + 4 * x + 4, wi
   testCoefficientC(dataSeries, 4)
   dataSeries.push(6, 173)
   testCoefficientA(dataSeries, 4.044444444444444)
-  testCoefficientB(dataSeries, 4)
+  testCoefficientB(dataSeries, 3.8222222222222215)
   testCoefficientC(dataSeries, 4)
   dataSeries.push(7, 227)
   testCoefficientA(dataSeries, 4)
