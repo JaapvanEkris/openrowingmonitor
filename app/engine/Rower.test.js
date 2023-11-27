dabd8de12cd3d9618bbf8844df97036a060f9cde
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2023-11-27 15:38:35 +0100

    Update Rower.test.js

--- app/engine/Rower.test.js
+++ app/engine/Rower.test.js
@@ -197,7 +197,7 @@ test('Correct Rower behaviour for three noisefree strokes with dynamic dragfacto
   testTotalMovingTimeSinceStart(rower, 0.46020725100000004)
   testTotalLinearDistanceSinceStart(rower, 1.6062393665262986)
   testTotalNumberOfStrokes(rower, 2)
-  testCycleDuration(rower, 0.404798464)
+  testCycleDuration(rower, 0.37123676400000005)
   testCycleLinearDistance(rower, 1.0716565176674184)
   testCycleLinearVelocity(rower, 3.2373328057193076)
   testCyclePower(rower, 94.99922799454356)
