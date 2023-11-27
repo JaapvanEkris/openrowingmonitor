23d1d581ec30c29072f95921648fae36afdef528
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2023-11-27 16:38:23 +0100

    Update BinarySearchTree.js

--- app/engine/utils/BinarySearchTree.js
+++ app/engine/utils/BinarySearchTree.js
@@ -1,73 +1,177 @@
-// Javascript function to search a given key in a given BST
+/* JavaScript program to find the median of BST in O(n) time and O(1) space*/
 
-class Node {
-  constructor(key) {
-    this.key = key
-    this.left = null
-    this.right = null
-  }
-}
-
-// A utility function to insert
-// a new node with given key in BST
-function insert(node, key) {
-  // If the tree is empty, return a new node
-  if (node === null) {
-	  return new Node(key)
-  }
-
-  // Otherwise, recur down the tree
-  if (key < node.key) {
-	  node.left = insert(node.left, key)
-  } else if (key > node.key) {
-	  node.right = insert(node.right, key)
-  }
-
-  // Return the (unchanged) node pointer
-  return node;
-  }
-
-  // Utility function to search a key in a BST
-  function search(root, key) {
-  // Base Cases: root is null or key is present at root
-  if (root === null || root.key === key) {
-    return root
-  }
-
-  // Key is greater than root's key
-  if (root.key < key) {
-	  return search(root.right, key)
-  }
-
-  // Key is smaller than root's key
-  return search(root.left, key)
-  }
-
-// Driver Code
-let root = null
-root = insert(root, 50)
-insert(root, 30)
-insert(root, 20)
-insert(root, 40)
-insert(root, 70)
-insert(root, 60)
-insert(root, 80)
-
-// Key to be found
-  let key = 6
-
-// Searching in a BST
-if (search(root, key) === null) {
-  console.log(key + " not found")
-} else {
-  console.log(key + " found")
-}
-
-key = 60
-
-// Searching in a BST
-if (search(root, key) === null) {
-  console.log(key + " not found")
-} else {
-  console.log(key + " found")
+/* A binary search tree Node has data, pointer 
+to left child and a pointer to right child */
+     class Node {
+            constructor() {
+                this.data = 0;
+                this.left = null;
+                this.right = null;
+            }
+        }
+ 
+// A utility function to create a new BST node 
+function newNode(item) 
+{ 
+    var temp = new Node(); 
+    temp.data = item; 
+    temp.left = null;
+    temp.right = null; 
+    return temp; 
+} 
+ 
+/* A utility function to insert a new node with 
+given key in BST */
+function insert(node , key) 
+{ 
+    /* If the tree is empty, return a new node */
+    if (node == null) return newNode(key); 
+ 
+    /* Otherwise, recur down the tree */
+    if (key < node.data) 
+        node.left = insert(node.left, key); 
+    else if (key > node.data) 
+        node.right = insert(node.right, key); 
+ 
+    /* return the (unchanged) node pointer */
+    return node; 
+} 
+ 
+/* Function to count nodes in a binary search tree 
+using Morris Inorder traversal*/
+function counNodes(root) 
+{ 
+    var current, pre; 
+ 
+    // Initialise count of nodes as 0 
+    var count = 0; 
+ 
+    if (root == null) 
+        return count; 
+ 
+    current = root; 
+    while (current != null) 
+    { 
+        if (current.left == null) 
+        { 
+            // Count node if its left is NULL 
+            count++; 
+ 
+            // Move to its right 
+            current = current.right; 
+        } 
+        else
+        { 
+            /* Find the inorder predecessor of current */
+            pre = current.left; 
+ 
+            while (pre.right != null && 
+                pre.right != current) 
+                pre = pre.right; 
+ 
+            /* Make current as right child of its 
+            inorder predecessor */
+            if(pre.right == null) 
+            { 
+                pre.right = current; 
+                current = current.left; 
+            } 
+ 
+            /* Revert the changes made in if part to 
+            restore the original tree i.e., fix 
+            the right child of predecessor */
+            else
+            { 
+                pre.right = null; 
+ 
+                // Increment count if the current 
+                // node is to be visited 
+                count++; 
+                current = current.right; 
+            } /* End of if condition pre->right == NULL */
+        } /* End of if condition current->left == NULL*/
+    } /* End of while */
+ 
+    return count; 
+} /* Function to find median in O(n) time and O(1) space 
+using Morris Inorder traversal*/
+function findMedian(root) 
+{ 
+if (root == null) 
+        return 0; 
+ 
+    var count = counNodes(root); 
+    var currCount = 0; 
+    var current = root, pre = null, prev = null; 
+ 
+    while (current != null) 
+    { 
+        if (current.left == null) 
+        { 
+            // count current node 
+            currCount++; 
+ 
+            // check if current node is the median 
+            // Odd case 
+            if (count % 2 != 0 && 
+            currCount == (count+1)/2) 
+                return current.data; 
+ 
+            // Even case 
+            else if (count % 2 == 0 && 
+            currCount == (count/2)+1) 
+                return (prev.data + current.data)/2; 
+ 
+            // Update prev for even no. of nodes 
+            prev = current; 
+ 
+            //Move to the right 
+            current = current.right; 
+        } 
+        else
+        { 
+            /* Find the inorder predecessor of current */
+            pre = current.left; 
+            while (pre.right != null && 
+            pre.right != current) 
+                pre = pre.right; 
+ 
+            /* Make current as right child of its
+            inorder predecessor */
+            if (pre.right == null) 
+            { 
+                pre.right = current; 
+                current = current.left; 
+            } 
+ 
+            /* Revert the changes made in if
+            part to restore the original 
+            tree i.e., fix the right child of predecessor */
+            else
+            { 
+                pre.right = null; 
+ 
+                prev = pre; 
+ 
+                // Count current node 
+                currCount++; 
+ 
+                // Check if the current node is the median 
+                if (count % 2 != 0 && 
+                currCount == (count+1)/2 ) 
+                    return current.data; 
+ 
+                else if (count%2==0 && 
+                currCount == (count/2)+1) 
+                    return (prev.data+current.data)/2; 
+ 
+                // update prev node for the case of even 
+                // no. of nodes 
+                prev = current; 
+                current = current.right; 
+ 
+            } /* End of if condition pre->right == NULL */
+        } /* End of if condition current->left == NULL*/
+    } /* End of while */
+    return -1;
 }
