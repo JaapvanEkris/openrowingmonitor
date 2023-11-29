83cf516d1c2ca5d77313abc06c221370310a2430
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2023-11-29 21:19:28 +0100

    Update Rower.test.js

--- app/engine/Rower.test.js
+++ app/engine/Rower.test.js
@@ -305,7 +305,7 @@ test('Correct Rower behaviour for three noisefree strokes with dynamic dragfacto
   testDriveLinearDistance(rower, 0.7485561895845304)
   testDriveLength(rower, 0.3141592653589791)
   testDriveAverageHandleForce(rower, 221.35842669540312)
-  testDrivePeakHandleForce(rower, 294.67568796679427)
+  testDrivePeakHandleForce(rower, 327.52410223150326)
   testRecoveryDuration(rower, 0.22775104700000026)
   testRecoveryDragFactor(rower, 303.0202258405183)
   testInstantHandlePower(rower, 0)
