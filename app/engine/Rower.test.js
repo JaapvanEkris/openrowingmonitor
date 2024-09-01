d6fff8e65b89de7d1a74c5647890acba54690541
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2024-09-01 18:18:45 +0200

    Update Rower.test.js

--- app/engine/Rower.test.js
+++ app/engine/Rower.test.js
@@ -174,7 +174,7 @@ test('Test behaviour for three perfect identical strokes, including settingling 
   testTotalNumberOfStrokes(rower, 2)
   testCycleDuration(rower, 0.34889498300000005)
   testCycleLinearDistance(rower, 1.1154667088547667)
-  testCycleLinearVelocity(rower, 3.3671261270026562)
+  testCycleLinearVelocity(rower, 3.2650920019419694)
   testCyclePower(rower, 106.8897805121993)
   testDriveDuration(rower, 0.143485717)
   testDriveLinearDistance(rower, 0.43908201661387286)
