e15d8e57f8e6d1975fd4b3622254e91bd047c9a7
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2024-09-01 18:18:12 +0200

    Update RowingStatistics.test.js

--- app/engine/RowingStatistics.test.js
+++ app/engine/RowingStatistics.test.js
@@ -179,7 +179,7 @@ test('Test behaviour for three perfect identical strokes, including settingling 
   rowingStatistics.handleRotationImpulse(0.010386684)
   testStrokeState(rowingStatistics, 'Drive')
   testTotalMovingTime(rowingStatistics, 0.44915539800000004)
-  testTotalLinearDistance(rowingStatistics, 1.6138554294533207)
+  testTotalLinearDistance(rowingStatistics, 1.5782562351248521)
   testTotalNumberOfStrokes(rowingStatistics, 1)
   testCycleDuration(rowingStatistics, undefined)
   testCycleDistance(rowingStatistics, undefined)
