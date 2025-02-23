4b5830ace840be6bbab98d5ef9482c2d31f0fbb0
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2025-02-23 13:29:33 +0100

    Update to new recorder architecture

--- app/recorders/rawRecorder.js
+++ app/recorders/rawRecorder.js
@@ -2,17 +2,15 @@
 /*
   Open Rowing Monitor, https://github.com/JaapvanEkris/openrowingmonitor
 
-  This Module captures the metrics of a rowing session and persists them.
+  This Module captures the raw pulses of a rowing session and persists them.
 */
 import log from 'loglevel'
-import zlib from 'zlib'
-import fs from 'fs/promises'
-import { promisify } from 'util'
-const gzip = promisify(zlib.gzip)
 
 export function createRawRecorder (config) {
+  const type = 'csv'
+  const postfix = '_raw'
+  const presentationName = 'Raw data'
   let rotationImpulses = []
-  let filename
   let allDataHasBeenWritten
 
   // This function handles all incomming commands. Here, the recordingmanager will have filtered
@@ -22,23 +20,15 @@ export function createRawRecorder (config) {
       case ('updateIntervalSettings'):
         break
       case ('reset'):
-        await createRawDataFile()
         rotationImpulses = null
         rotationImpulses = []
-        filename = undefined
         allDataHasBeenWritten = true
         break
       case 'shutdown':
-        await createRawDataFile()
         break
       default:
         log.error(`rawRecorder: Recieved unknown command: ${commandName}`)
     }
-  }
-
-  function setBaseFileName (baseFileName) {
-    filename = `${baseFileName}_raw.csv${config.gzipRawDataFiles ? '.gz' : ''}`
-    log.info(`Raw data file will be saved as ${filename} (after the session)`)
   }
 
   async function recordRotationImpulse (impulse) {
@@ -51,69 +41,68 @@ export function createRawRecorder (config) {
   function recordRowingMetrics (metrics) {
     switch (true) {
       case (metrics.metricsContext.isSessionStop):
-        createRawDataFile()
-        setTimeout(recreateRawDataFile, 60000)
         break
       case (metrics.metricsContext.isPauseStart):
-        createRawDataFile()
-        setTimeout(recreateRawDataFile, 60000)
         break
     }
   }
 
-  function recreateRawDataFile () {
-    // This function is called when the rowing session is stopped.
-    if (allDataHasBeenWritten) return
-
-    // Some data has been added after the last write
-    createRawDataFile()
-    setTimeout(recreateRawDataFile, 60000)
-  }
-
-  async function createRawDataFile () {
-    // Do not write again if not needed
-    if (allDataHasBeenWritten) return
-
-    // we need at least two strokes and ten seconds to generate a valid tcx file
-    if (!minimumRecordingTimeHasPassed()) {
-      log.info('raw file has not been written, as there was not enough data recorded (minimum 10 seconds)')
-      return
-    }
-
-    await createFile(rotationImpulses.join('\n'), filename, config.gzipRawDataFiles)
-
-    allDataHasBeenWritten = true
-    log.info(`Raw data has been written as ${filename}`)
-  }
-
-  async function createFile (content, filename, compress = false) {
-    if (compress) {
-      const gzipContent = await gzip(content)
-      try {
-        await fs.writeFile(filename, gzipContent)
-      } catch (err) {
-        log.error(err)
-      }
+  async function fileContent () {
+    const rawData = rotationImpulses.join('\n')
+    if (rawData === undefined) {
+      log.error('error creating raw file content')
+      return undefined
     } else {
-      try {
-        await fs.writeFile(filename, content)
-      } catch (err) {
-        log.error(err)
-      }
+      return rawData
     }
   }
 
-  function minimumRecordingTimeHasPassed () {
+  function minimumDataAvailable () {
     const minimumRecordingTimeInSeconds = 10
     // We need to make sure that we use the Math.abs(), as a gpio rollover can cause impulse to be negative!
     const rotationImpulseTimeTotal = rotationImpulses.reduce((acc, impulse) => acc + Math.abs(impulse), 0)
     return (rotationImpulseTimeTotal > minimumRecordingTimeInSeconds)
   }
 
+  function totalRecordedDistance () {
+    return 0
+  }
+
+  function totalRecordedMovingTime () {
+    const rotationImpulseTimeTotal = rotationImpulses.reduce((acc, impulse) => acc + Math.abs(impulse), 0)
+    if (rotationImpulseTimeTotal > 0) {
+      return rotationImpulseTimeTotal
+    } else {
+      return 0
+    }
+  }
+
+  function sessionDrag () {
+    return 0
+  }
+
+  function sessionVO2Max () {
+    return undefined
+  }
+
+  function sessionHRR () {
+    return []
+  }
+
   return {
-    setBaseFileName,
     recordRotationImpulse,
     recordRowingMetrics,
-    handleCommand
+    handleCommand,
+    minimumDataAvailable,
+    fileContent,
+    type,
+    postfix,
+    presentationName,
+    totalRecordedDistance,
+    totalRecordedMovingTime,
+    sessionDrag,
+    sessionVO2Max,
+    sessionHRR,
+    allDataHasBeenWritten
   }
 }
