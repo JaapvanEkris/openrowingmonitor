23cce7680f0b7c465f52f69aae18d4a94415c211
Abász <32517724+Abasz@users.noreply.github.com>
2025-03-17 10:42:27 +0100

    Add logging to staticReadCharacteristic
    Add logging to the staticReadCharacteristic factory to improve debugging

--- app/peripherals/ble/common/StaticReadCharacteristic.js
+++ app/peripherals/ble/common/StaticReadCharacteristic.js
@@ -2,7 +2,10 @@
 /*
   Open Rowing Monitor, https://github.com/JaapvanEkris/openrowingmonitor
 */
+import NodeBleHost from 'ble-host'
+import loglevel from 'loglevel'
 
+const log = loglevel.getLogger('Peripherals')
 export function createStaticReadCharacteristic (uuid, value, description, addNotify = false) {
   const descriptors = description !== undefined
     ? [
@@ -12,10 +15,20 @@ export function createStaticReadCharacteristic (uuid, value, description, addNot
         }]
     : undefined
 
+  const onSubscriptionChange = addNotify
+    ? (connection, notification) => {
+        log.debug(`${description !== undefined ? description : uuid} subscription change: ${connection.peerAddress}, notification: ${notification}`)
+      }
+    : undefined
+
   return {
     uuid,
     properties: addNotify ? ['read', 'notify'] : ['read'],
-    value: Buffer.isBuffer(value) ? value : Buffer.from(value),
-    descriptors
+    descriptors,
+    onRead: (connection, callback) => {
+      log.debug(`Static read characteristic has been called: ${description}`)
+      callback(NodeBleHost.AttErrors.SUCCESS, Buffer.isBuffer(value) ? value : Buffer.from(value))
+    },
+    onSubscriptionChange
   }
 }
