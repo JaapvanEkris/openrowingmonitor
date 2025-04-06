8b2eb337f6ee553b4d7c94536960f924c9e6f90a
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2025-04-06 22:07:29 +0200

    Adaptation to new workoutSegment setup

--- app/peripherals/ble/pm5/rowing-service/session-characteristics/WorkoutSummaryCharacteristic.js
+++ app/peripherals/ble/pm5/rowing-service/session-characteristics/WorkoutSummaryCharacteristic.js
@@ -31,7 +31,7 @@ export class WorkoutSummaryCharacteristic extends GattNotifyCharacteristic {
    * @param {SegmentMetrics} workoutData
    */
   // @ts-ignore: Type is not assignable to type
-  notify (data, workoutData) {
+  notify (data, workoutHRData) {
     const bufferBuilder = new BufferBuilder()
     // Data bytes packed as follows: (20bytes) example: 0333 1212 A0A500 102700 0C 00 00 00 00 8D 00 07 4808
 
@@ -39,30 +39,29 @@ export class WorkoutSummaryCharacteristic extends GattNotifyCharacteristic {
     bufferBuilder.writeUInt16LE(new Concept2Date().toC2DateInt())
     // Log Entry Time (see https://www.c2forum.com/viewtopic.php?t=200769)
     bufferBuilder.writeUInt16LE(new Concept2Date().toC2TimeInt())
-    // Elapsed Time Lo (0.01 sec lsb),
-    bufferBuilder.writeUInt24LE(Math.round(workoutData.totalTime() * 100))
-    // Distance Lo (0.1 m)
-    bufferBuilder.writeUInt24LE(workoutData.traveledLinearDistance() > 0 ? Math.round(workoutData.traveledLinearDistance() * 10) : 0)
+    // Elapsed Time (0.01 sec lsb),
+    bufferBuilder.writeUInt24LE(Math.round(data.workout.timeSpent.total * 100))
+    // Distance (0.1 m)
+    bufferBuilder.writeUInt24LE(data.workout.distance.fromStart > 0 ? Math.round(data.workout.distance.fromStart * 10) : 0)
     // Average Stroke Rate,
-    bufferBuilder.writeUInt8(workoutData.strokerate.average() > 0 && workoutData.strokerate.average() < 255 ? Math.round(workoutData.strokerate.average()) : 0)
+    bufferBuilder.writeUInt8(data.workout.strokerate.average > 0 && data.workout.strokerate.average < 255 ? Math.round(data.workout.strokerate.average) : 0)
     // Ending Heartrate,
     bufferBuilder.writeUInt8(workoutData.heartrate.atSeriesEnd() > 0 ? Math.round(workoutData.heartrate.atSeriesEnd()) : 0)
     // Average Heartrate,
-    bufferBuilder.writeUInt8(workoutData.heartrate.average() > 0 ? Math.round(workoutData.heartrate.average()) : 0)
+    bufferBuilder.writeUInt8(workoutHRData.average() > 0 ? Math.round(workoutHRData.average()) : 0)
     // Min Heartrate,
-    bufferBuilder.writeUInt8(workoutData.heartrate.minimum() > 0 ? Math.round(workoutData.heartrate.minimum()) : 0)
+    bufferBuilder.writeUInt8(workoutHRData.minimum() > 0 ? Math.round(workoutHRData.minimum()) : 0)
     // Max Heartrate,
-    bufferBuilder.writeUInt8(workoutData.heartrate.maximum() > 0 ? Math.round(workoutData.heartrate.maximum()) : 0)
+    bufferBuilder.writeUInt8(workoutHRData.maximum() > 0 ? Math.round(workoutHRData.maximum()) : 0)
     // Drag Factor Average,
-    bufferBuilder.writeUInt8(workoutData.dragFactor.average() > 0 && workoutData.dragFactor.average() < 255 ? Math.round(workoutData.dragFactor.average()) : 255)
+    bufferBuilder.writeUInt8(data.workout.dragfactor.average > 0 && data.workout.dragfactor.average < 255 ? Math.round(data.workout.dragfactor.average) : 255)
     // Recovery Heart Rate, (zero = not valid data. After 1 minute of rest/recovery, PM5 sends this data as a revised End Of Workout summary data characteristic unless the monitor has been turned off or a new workout started)
     bufferBuilder.writeUInt8(0)
     // Workout Type,
-    bufferBuilder.writeUInt8(SessionTypes[data.sessiontype] ?? SessionTypes.justrow)
+    bufferBuilder.writeUInt8(SessionTypes[data.workout.type] ?? SessionTypes.justrow)
     if (this.isSubscribed) {
-    // Avg Pace Lo (0.1 sec lsb) - NOT IN MULTIPLEXED
-    // Avg Pace Hi - NOT IN MULTIPLEXED
-      bufferBuilder.writeUInt16LE(Math.round(workoutData.pace.average()))
+    // Avg Pace (0.1 sec) - NOT IN MULTIPLEXED
+      bufferBuilder.writeUInt16LE(Math.round(data.workout.pace.average))
     }
 
     if (this.isSubscribed) {
