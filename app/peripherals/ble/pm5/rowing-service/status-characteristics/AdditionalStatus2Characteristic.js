b84d5d0dd2e0f8e3a7982ae2a0328d36393461eb
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2025-04-21 00:37:48 +0200

    Improvement of the PM5 metrics

--- app/peripherals/ble/pm5/rowing-service/status-characteristics/AdditionalStatus2Characteristic.js
+++ app/peripherals/ble/pm5/rowing-service/status-characteristics/AdditionalStatus2Characteristic.js
@@ -3,7 +3,8 @@
   Open Rowing Monitor, https://github.com/JaapvanEkris/openrowingmonitor
 
   Implementation of the AdditionalStatus2 as defined in:
-  https://www.concept2.co.uk/files/pdf/us/monitors/PM5_BluetoothSmartInterfaceDefinition.pdf
+  * https://www.concept2.co.uk/files/pdf/us/monitors/PM5_BluetoothSmartInterfaceDefinition.pdf
+  * https://www.concept2.co.uk/files/pdf/us/monitors/PM5_CSAFECommunicationDefinition.pdf
 */
 import { BufferBuilder } from '../../../BufferBuilder.js'
 import { GattNotifyCharacteristic } from '../../../BleManager.js'
@@ -35,9 +36,9 @@ export class AdditionalStatus2Characteristic extends GattNotifyCharacteristic {
   notify (data, previousSplitData) {
     const bufferBuilder = new BufferBuilder()
     // elapsedTime: UInt24LE in 0.01 sec
-    bufferBuilder.writeUInt24LE(Math.round(data.totalMovingTime * 100))
+    bufferBuilder.writeUInt24LE(data.workout.timeSpent.total > 0 ? Math.round(data.workout.timeSpent.total * 100) : 0)
     // intervalCount: UInt8
-    bufferBuilder.writeUInt8(data.splitNumber)
+    bufferBuilder.writeUInt8(data.split.number > 0 ? data.split.number : 0)
     if (this.isSubscribed) {
       // the multiplexer uses a slightly different format for the AdditionalStatus2
       // it skips averagePower before totalCalories
@@ -45,15 +46,15 @@ export class AdditionalStatus2Characteristic extends GattNotifyCharacteristic {
       bufferBuilder.writeUInt16LE(data.cyclePower > 0 ? Math.round(data.cyclePower) : 0)
     }
     // totalCalories: UInt16LE in kCal
-    bufferBuilder.writeUInt16LE(data.totalCalories > 0 ? Math.round(data.totalCalories) : 0)
+    bufferBuilder.writeUInt16LE(data.workout.calories.totalSpent > 0 ? Math.round(data.workout.calories.totalSpent) : 0)
     // splitAveragePace: UInt16LE in 0.01 sec/500m
-    bufferBuilder.writeUInt16LE(data.split.pace.average && data.split.pace.average > 0 && data.split.pace.average < 655.34 ? Math.round(data.split.pace.average * 100) : 0xFFFF)
+    bufferBuilder.writeUInt16LE(data.split.pace.average && data.split.pace.average > 0 && data.split.pace.average < 655.34 ? Math.round(data.split.pace.average * 100) : 0)
     // splitAveragePower UInt16LE in watts
     bufferBuilder.writeUInt16LE(data.split.power.average > 0 && data.split.power.average < 65534 ? Math.round(data.split.power.average) : 0)
     // splitAverageCalories
     bufferBuilder.writeUInt16LE(data.split.calories.averagePerHour > 0 && data.split.calories.averagePerHour < 65534 ? Math.round(data.split.calories.averagePerHour) : 0)
-    // lastSplitTime
-    bufferBuilder.writeUInt24LE(previousSplitData.totalMovingTime > 0 ? Math.round(previousSplitData.totalMovingTime * 100) : 0)
+    // lastSplitTime in 0.1 sec
+    bufferBuilder.writeUInt24LE(previousSplitData.totalMovingTime > 0 ? Math.round(previousSplitData.totalMovingTime * 10) : 0)
     // lastSplitDistance in 1 m
     bufferBuilder.writeUInt24LE(previousSplitData.totalLinearDistance > 0 ? Math.round(previousSplitData.totalLinearDistance) : 0)
 
