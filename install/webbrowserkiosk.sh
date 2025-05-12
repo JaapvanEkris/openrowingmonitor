35012a85c10b9f8acf5e876288257d5a5299f30e
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2025-05-12 18:17:22 +0200

    Moved to Firefox

--- install/webbrowserkiosk.sh
+++ install/webbrowserkiosk.sh
@@ -2,14 +2,12 @@
 #
 #  Open Rowing Monitor, https://github.com/JaapvanEkris/openrowingmonitor
 #
-#  Runs the Web Frontend in a chromium browser in fullscreen kiosk mode
+#  Runs the Web Frontend in a Firefox browser in fullscreen kiosk mode
 #
 xset s off
 xset s noblank
 xset -dpms
 openbox-session &
 
-# Start Chromium in kiosk mode
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/'Local State'
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
-chromium-browser --disable-infobars --disable-features=AudioServiceSandbox --kiosk --noerrdialogs --ignore-certificate-errors --disable-session-crashed-bubble --disable-pinch --enable-low-end-device-mode --disable-site-isolation-trials --renderer-process-limit=2 --check-for-update-interval=604800 --app="http://127.0.0.1/?mode=kiosk"
+# Start Firefox in kiosk mode
+firefox --display=:0 --kiosk-monitor 0 --kiosk http://127.0.0.1/?mode=kiosk
