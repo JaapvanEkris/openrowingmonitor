caca940b4ba0efd1a8a4169720e950a3ef967b3a
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2025-05-27 22:15:38 +0200

    Fix for crash at split rollover

--- app/peripherals/ble/pm5/rowing-service/session-characteristics/SplitDataCharacteristic.js
+++ app/peripherals/ble/pm5/rowing-service/session-characteristics/SplitDataCharacteristic.js
@@ -39,7 +39,7 @@ export class SplitDataCharacteristic extends GattNotifyCharacteristic {
     // Elapsed Time (0.01 sec),
     bufferBuilder.writeUInt24LE(data.interval.timeSpent.moving > 0 ? Math.round(data.interval.timeSpent.moving * 100) : 0)
     // Distance in split (0.1 m), based on experiments with the intervals screen
-    bufferBuilder.writeUInt24LE(data.split.interval.fromStart > 0 ? Math.round(data.interval.distance.fromStart * 10) : 0)
+    bufferBuilder.writeUInt24LE(data.interval.distance.fromStart > 0 ? Math.round(data.interval.distance.fromStart * 10) : 0)
     // Split/Interval Time (0.1 sec)
     bufferBuilder.writeUInt24LE(data.split.timeSpent.moving > 0 ? Math.round(data.split.timeSpent.moving * 10) : 0)
     // Split/Interval Distance (1m accurate)
