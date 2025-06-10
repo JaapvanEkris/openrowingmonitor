8d41a8e85de163e5fbb9fe170e0d30a1c2531052
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2025-06-10 13:45:28 +0200

    Improved robustness during startup

--- app/client/store/dashboardMetrics.js
+++ app/client/store/dashboardMetrics.js
@@ -18,45 +18,45 @@ export const DASHBOARD_METRICS = {
     template: (metrics, config) => {
       let distance
       switch (true) {
-        case (metrics?.interval.type === 'rest' && metrics?.pauseCountdownTime > 0):
+        case (metrics?.interval?.type === 'rest' && metrics?.pauseCountdownTime > 0):
           distance = 0
           break
         case (metrics?.interval.type === 'distance'):
-          distance = Math.max(metrics?.interval.distance.toEnd, 0)
+          distance = Math.max(metrics?.interval?.distance?.toEnd, 0)
           break
         default:
-          distance = Math.max(metrics?.interval.distance.fromStart, 0)
+          distance = Math.max(metrics?.interval?.distance?.fromStart, 0)
       }
       const linearDistance = formatDistance(distance ?? 0)
 
-      return simpleMetricFactory(linearDistance.distance, linearDistance.unit, config.guiConfigs.showIcons ? iconRoute : '')
+      return simpleMetricFactory(linearDistance.distance, linearDistance.unit, config?.guiConfigs?.showIcons ? iconRoute : '')
     }
   },
 
-  pace: { displayName: 'Pace/500', size: 1, template: (metrics, config) => simpleMetricFactory(secondsToTimeString(metrics?.cyclePace), '/500m', config.guiConfigs.showIcons ? iconStopwatch : '') },
+  pace: { displayName: 'Pace/500', size: 1, template: (metrics, config) => simpleMetricFactory(secondsToTimeString(metrics?.cyclePace), '/500m', config?.guiConfigs?.showIcons ? iconStopwatch : '') },
 
-  power: { displayName: 'Power', size: 1, template: (metrics, config) => simpleMetricFactory(formatNumber(metrics?.cyclePower), 'watt', config.guiConfigs.showIcons ? iconBolt : '') },
+  power: { displayName: 'Power', size: 1, template: (metrics, config) => simpleMetricFactory(formatNumber(metrics?.cyclePower), 'watt', config?.guiConfigs?.showIcons ? iconBolt : '') },
 
   stkRate: { displayName: 'Stroke rate', size: 1, template: (metrics, config) => simpleMetricFactory(formatNumber(metrics?.cycleStrokeRate), '/min', config.guiConfigs.showIcons ? iconPaddle : '') },
   heartRate: {
     displayName: 'Heart rate',
     size: 1,
-    template: (metrics, config) => html`<dashboard-metric .icon=${config.guiConfigs.showIcons ? iconHeartbeat : ''} unit="bpm" .value=${formatNumber(metrics?.heartrate)}>
+    template: (metrics, config) => html`<dashboard-metric .icon=${config?.guiConfigs?.showIcons ? iconHeartbeat : ''} unit="bpm" .value=${formatNumber(metrics?.heartrate)}>
       ${metrics?.heartRateBatteryLevel > 0 ?
         html`<battery-icon .batteryLevel=${metrics?.heartRateBatteryLevel}></battery-icon>` :
         ''}
     </dashboard-metric>`
   },
 
-  totalStk: { displayName: 'Total strokes', size: 1, template: (metrics, config) => simpleMetricFactory(metrics?.totalNumberOfStrokes, 'stk', config.guiConfigs.showIcons ? iconPaddle : '') },
+  totalStk: { displayName: 'Total strokes', size: 1, template: (metrics, config) => simpleMetricFactory(metrics?.totalNumberOfStrokes, 'stk', config?.guiConfigs?.showIcons ? iconPaddle : '') },
 
   calories: {
     displayName: 'Calories',
     size: 1,
     template: (metrics, config) => {
-      const calories = metrics?.interval.type === 'Calories' ? Math.max(metrics?.interval.TargetCalories - metrics?.interval.Calories, 0) : metrics?.totalCalories
+      const calories = metrics?.interval.type === 'Calories' ? Math.max(metrics?.interval?.TargetCalories - metrics?.interval?.Calories, 0) : metrics?.totalCalories
 
-      return simpleMetricFactory(formatNumber(calories ?? 0), 'kcal', config.guiConfigs.showIcons ? iconFire : '')
+      return simpleMetricFactory(formatNumber(calories ?? 0), 'kcal', config?.guiConfigs?.showIcons ? iconFire : '')
     }
   },
 
@@ -67,32 +67,32 @@ export const DASHBOARD_METRICS = {
       let time
       let icon
       switch (true) {
-        case (metrics?.interval.type === 'rest' && metrics?.pauseCountdownTime > 0):
+        case (metrics?.interval?.type === 'rest' && metrics?.pauseCountdownTime > 0):
           time = metrics?.pauseCountdownTime
           icon = iconAlarmclock
           break
-        case (metrics?.interval.type === 'time'):
-          time = Math.max(metrics?.interval.movingTime.toEnd, 0)
+        case (metrics?.interval?.type === 'time'):
+          time = Math.max(metrics?.interval?.movingTime?.toEnd, 0)
           icon = iconClock
           break
         default:
-          time = Math.max(metrics?.interval.movingTime.sinceStart, 0)
+          time = Math.max(metrics?.interval?.movingTime?.sinceStart, 0)
           icon = iconClock
       }
 
-      return simpleMetricFactory(secondsToTimeString(time ?? 0), '', config.guiConfigs.showIcons ? icon : '')
+      return simpleMetricFactory(secondsToTimeString(time ?? 0), '', config?.guiConfigs?.showIcons ? icon : '')
     }
   },
 
-  distancePerStk: { displayName: 'Dist per Stroke', size: 1, template: (metrics, config) => simpleMetricFactory(formatNumber(metrics?.cycleDistance, 1), 'm', config.guiConfigs.showIcons ? rowerIcon : '') },
+  distancePerStk: { displayName: 'Dist per Stroke', size: 1, template: (metrics, config) => simpleMetricFactory(formatNumber(metrics?.cycleDistance, 1), 'm', config?.guiConfigs?.showIcons ? rowerIcon : '') },
 
-  dragFactor: { displayName: 'Drag factor', size: 1, template: (metrics, config) => simpleMetricFactory(formatNumber(metrics?.dragFactor), '', config.guiConfigs.showIcons ? 'Drag' : '') },
+  dragFactor: { displayName: 'Drag factor', size: 1, template: (metrics, config) => simpleMetricFactory(formatNumber(metrics?.dragFactor), '', config?.guiConfigs?.showIcons ? 'Drag' : '') },
 
-  driveLength: { displayName: 'Drive length', size: 1, template: (metrics, config) => simpleMetricFactory(formatNumber(metrics?.driveLength, 2), 'm', config.guiConfigs.showIcons ? 'Drive' : '') },
+  driveLength: { displayName: 'Drive length', size: 1, template: (metrics, config) => simpleMetricFactory(formatNumber(metrics?.driveLength, 2), 'm', config?.guiConfigs?.showIcons ? 'Drive' : '') },
 
-  driveDuration: { displayName: 'Drive duration', size: 1, template: (metrics, config) => simpleMetricFactory(formatNumber(metrics?.driveDuration, 2), 'sec', config.guiConfigs.showIcons ? 'Drive' : '') },
+  driveDuration: { displayName: 'Drive duration', size: 1, template: (metrics, config) => simpleMetricFactory(formatNumber(metrics?.driveDuration, 2), 'sec', config?.guiConfigs?.showIcons ? 'Drive' : '') },
 
-  recoveryDuration: { displayName: 'Recovery duration', size: 1, template: (metrics, config) => simpleMetricFactory(formatNumber(metrics?.recoveryDuration, 2), 'sec', config.guiConfigs.showIcons ? 'Recovery' : '') },
+  recoveryDuration: { displayName: 'Recovery duration', size: 1, template: (metrics, config) => simpleMetricFactory(formatNumber(metrics?.recoveryDuration, 2), 'sec', config?.guiConfigs?.showIcons ? 'Recovery' : '') },
 
   forceCurve: { displayName: 'Force curve', size: 2, template: (metrics) => html`<dashboard-force-curve .value=${metrics?.driveHandleForceCurve} style="grid-column: span 2"></dashboard-force-curve>` },
 
