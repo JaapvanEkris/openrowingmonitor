deca19afd5874d1987768d54ae2e7a8298ff2fbc
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2025-08-05 17:23:53 +0200

    Update FullTSQuadraticSeries.js

--- app/engine/utils/FullTSQuadraticSeries.js
+++ app/engine/utils/FullTSQuadraticSeries.js
@@ -186,7 +186,7 @@ export function createTSQuadraticSeries (maxSeriesLength = 0) {
       // eslint-disable-next-line no-console
       console.log(`recalculated GoF, _sst= ${_sst}`)
     }
-    if (X.length() >= 3 && _sst = 0) {
+    if (X.length() >= 3 && _sst === 0) {
         while (i < X.length()) {
           _sst += Math.pow((Y.get(i) - Y.average()), 2)
           i++
