c07ed9292f10ce6a04ee1fb95b5d1a403860d865
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2025-08-06 22:56:04 +0200

    Updated tests for the NSE algorithm

--- app/engine/Rower.test.js
+++ app/engine/Rower.test.js
@@ -181,7 +181,7 @@ test('Test behaviour for three perfect identical strokes, including settingling 
   testDriveLinearDistance(rower, 0.43908201661387253)
   testDriveLength(rower, 0.1759291886010284)
   testDriveAverageHandleForce(rower, 236.59565919132677)
-  testDrivePeakHandleForce(rower, 380.1396336099103)
+  testDrivePeakHandleForce(rower, 380.1396291775697)
   testRecoveryDuration(rower, 0.20540926600000003)
   testRecoveryDragFactor(rower, 283.12720365097886)
   testInstantHandlePower(rower, 504.63602120716615)
