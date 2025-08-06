478bb0d79833b49f96faa5d82bba9d8152193087
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2025-08-06 23:04:10 +0200

    Updated tests for the NSE algorithm

--- app/engine/Rower.test.js
+++ app/engine/Rower.test.js
@@ -212,7 +212,7 @@ test('Test behaviour for three perfect identical strokes, including settingling 
   testDriveDuration(rower, 0.23985939100000003)
   testDriveLinearDistance(rower, 1.0733115961672441)
   testDriveLength(rower, 0.322536845768552)
-  testDriveAverageHandleForce(rower, 285.0923064376231)
+  testDriveAverageHandleForce(rower, 285.0926572298846)
   testDrivePeakHandleForce(rower, 439.7407274840117)
   testRecoveryDuration(rower, 0.20540926600000003)
   testRecoveryDragFactor(rower, 283.12720365097886) // As we decelerate the flywheel quite fast, this is expected
