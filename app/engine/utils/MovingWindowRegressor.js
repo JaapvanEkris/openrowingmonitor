fa483ce3b189fee5921309e07302c11f51926b5c
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2025-08-30 00:32:18 +0200

    Generalisation of the algorithm

--- app/engine/utils/MovingWindowRegressor.js
+++ app/engine/utils/MovingWindowRegressor.js
@@ -3,8 +3,8 @@
   Open Rowing Monitor, https://github.com/JaapvanEkris/openrowingmonitor
 */
 /**
- * This implements a Moving Regression Algorithm to obtain a first (angular velocity) and
- * second derivative (angular acceleration) at a specific point in our series
+ * This implements a Moving Regression Algorithm to obtain a coefficients, first (angular velocity) and
+ * second derivative (angular acceleration) at the front of the flank
  */
 
 import { createTSQuadraticSeries } from './FullTSQuadraticSeries.js'
@@ -15,51 +15,82 @@ export function createMovingRegressor (bandwith) {
   const flankLength = bandwith
   const movingWindow = createTSQuadraticSeries(flankLength)
   const gausianWeight = createGausianWeightFunction()
-  let firstDerivativeMatrix = []
-  let secondDerivativeMatrix = []
+  let aMatrix = []
+  let bMatrix = []
+  let cMatrix = []
 
   function push (x, y) {
     movingWindow.push(x, y)
 
     // Let's shift the matrix to make room for a new datapoint
-    if (firstDerivativeMatrix.length >= flankLength) {
+    if (aMatrix.length >= flankLength) {
       // The angularVelocityMatrix has reached its maximum length, we need to remove the first element
-      firstDerivativeMatrix[0].reset()
-      firstDerivativeMatrix[0] = null
-      firstDerivativeMatrix.shift()
-      secondDerivativeMatrix[0].reset()
-      secondDerivativeMatrix[0] = null
-      secondDerivativeMatrix.shift()
+      aMatrix[0].reset()
+      aMatrix[0] = null
+      aMatrix.shift()
+      bMatrix[0].reset()
+      bMatrix[0] = null
+      bMatrix.shift()
+      cMatrix[0].reset()
+      cMatrix[0] = null
+      cMatrix.shift()
     }
 
     // Let's make room for a new set of values for first and second derivatives
-    firstDerivativeMatrix[firstDerivativeMatrix.length] = createWeighedSeries(flankLength, 0)
-    secondDerivativeMatrix[secondDerivativeMatrix.length] = createWeighedSeries(flankLength, 0)
+    aMatrix[aMatrix.length] = createWeighedSeries(flankLength, 0)
+    bMatrix[bMatrix.length] = createWeighedSeries(flankLength, 0)
+    cMatrix[cMatrix.length] = createWeighedSeries(flankLength, 0)
 
     let i = 0
-	let weight = 0
+    let weight = 0
     gausianWeight.setWindowWidth(movingWindow.X.atSeriesBegin(), movingWindow.X.atSeriesEnd())
 
     // Let's calculate the first and second derivatives for each datapoint and store them in their matrices
-    while (i < firstDerivativeMatrix.length) {
-	  weight = movingWindow.goodnessOfFit() * movingWindow.localGoodnessOfFit(i) * gausianWeight.weight(movingWindow.X.get(i))
-      firstDerivativeMatrix[i].push(movingWindow.firstDerivativeAtPosition(i), weight)
-      secondDerivativeMatrix[i].push(movingWindow.secondDerivativeAtPosition(i), weight)
+    while (i < aMatrix.length) {
+      weight = movingWindow.goodnessOfFit() * movingWindow.localGoodnessOfFit(i) * gausianWeight.weight(movingWindow.X.get(i))
+      aMatrix[i].push(movingWindow.coefficientA(), weight)
+      bMatrix[i].push(movingWindow.coefficientB(), weight)
+      cMatrix[i].push(movingWindow.coefficientC(), weight)
       i++
     }
   }
 
+  function coefficientAAtBeginFlank () {
+    if (aMatrix.length === flankLength) {
+      return aMatrix[0].weighedAverage()
+    } else {
+      return undefined
+    }
+  }
+
+  function coefficientBAtBeginFlank () {
+    if (bMatrix.length === flankLength) {
+      return bMatrix[0].weighedAverage()
+    } else {
+      return undefined
+    }
+  }
+
+  function coefficientCAtBeginFlank () {
+    if (cMatrix.length === flankLength) {
+      return cMatrix[0].weighedAverage()
+    } else {
+      return undefined
+    }
+  }
+
+
   function firstDerivativeAtBeginFlank () {
-    if (firstDerivativeMatrix.length === flankLength) {
-      return firstDerivativeMatrix[0].weighedAverage()
+    if (aMatrix.length === flankLength) {
+      return ((aMatrix[0].weighedAverage() * 2 * movingWindow.X.get(0)) + bMatrix[0].weighedAverage())
     } else {
       return undefined
     }
   }
 
   function secondDerivativeAtBeginFlank () {
-    if (firstDerivativeMatrix.length === flankLength) {
-      return secondDerivativeMatrix[0].weighedAverage()
+    if (aMatrix.length === flankLength) {
+      return (aMatrix[0].weighedAverage() * 2)
     } else {
       return undefined
     }
@@ -67,28 +98,42 @@ export function createMovingRegressor (bandwith) {
 
   function reset () {
     movingWindow.reset()
-    let i = firstDerivativeMatrix.length
+    let i = aMatrix.length
     while (i > 0) {
-      firstDerivativeMatrix[0].reset()
-      firstDerivativeMatrix[0] = null
-      firstDerivativeMatrix.shift()
+      aMatrix[0].reset()
+      aMatrix[0] = null
+      aMatrix.shift()
       i--
     }
-    firstDerivativeMatrix = null
-    firstDerivativeMatrix = []
-    let j = secondDerivativeMatrix.length
+    aMatrix = null
+    aMatrix = []
+
+    let j = bMatrix.length
     while (j > 0) {
-      secondDerivativeMatrix[0].reset()
-      secondDerivativeMatrix[0] = null
-      secondDerivativeMatrix.shift()
+      bMatrix[0].reset()
+      bMatrix[0] = null
+      bMatrix.shift()
       j--
     }
-    secondDerivativeMatrix = null
-    secondDerivativeMatrix = []
+    bMatrix = null
+    bMatrix = []
+
+    let k = cMatrix.length
+    while (k > 0) {
+      cMatrix[0].reset()
+      cMatrix[0] = null
+      cMatrix.shift()
+      k--
+    }
+    cMatrix = null
+    cMatrix = []
   }
 
   return {
     push,
+    coefficientAAtBeginFlank,
+    coefficientBAtBeginFlank,
+    coefficientCAtBeginFlank,
     firstDerivativeAtBeginFlank,
     secondDerivativeAtBeginFlank,
     reset
