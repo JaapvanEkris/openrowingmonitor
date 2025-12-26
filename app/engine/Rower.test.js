952a3ffcf27125ecf62dd3bbf4838dc187df5fbe
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2025-12-26 21:42:11 +0100

    Fix expected total linear distance in Rower tests

--- app/engine/Rower.test.js
+++ app/engine/Rower.test.js
@@ -431,7 +431,7 @@ test('A full session for a Concept2 RowErg should produce plausible results', as
   await replayRowingSession(rower.handleRotationImpulse, { filename: 'recordings/Concept2_RowErg_Session_2000meters.csv', realtime: false, loop: false })
 
   testTotalMovingTimeSinceStart(rower, 590.0294331572366)
-  testTotalLinearDistanceSinceStart(rower, 2027.8318516062236)
+  testTotalLinearDistanceSinceStart(rower, 2027.8951016561075)
   testTotalNumberOfStrokes(rower, 206)
   // As dragFactor isn't static, it should have changed
   testRecoveryDragFactor(rower, 80.68166392487412)
