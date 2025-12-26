94c9dcef44016aa98f80fc8c48da2586d63431c2
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2025-12-26 21:39:32 +0100

    Fix total moving time in rowing statistics test

--- app/engine/RowingStatistics.test.js
+++ app/engine/RowingStatistics.test.js
@@ -490,7 +490,7 @@ test('A full session for a Concept2 RowErg should produce plausible results', as
 
   await replayRowingSession(rowingStatistics.handleRotationImpulse, { filename: 'recordings/Concept2_RowErg_Session_2000meters.csv', realtime: false, loop: false })
 
-  testTotalMovingTime(rowingStatistics, 590.039795913105)
+  testTotalMovingTime(rowingStatistics, 590.0294331572366)
   testTotalLinearDistance(rowingStatistics, 2027.8318516062236)
   testTotalNumberOfStrokes(rowingStatistics, 205)
   // As dragFactor isn't static, it should have changed
