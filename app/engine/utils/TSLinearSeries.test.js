ec08f367088045f0ce264103c02e5bfc604e699c
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2026-01-02 20:18:26 +0100

    Update TSLinearSeries.test.js

--- app/engine/utils/TSLinearSeries.test.js
+++ app/engine/utils/TSLinearSeries.test.js
@@ -231,7 +231,7 @@ test('Series with 5 elements, with 2 noisy datapoints, ideal function y = 3x - 6
   testGoodnessOfFitEquals(dataSeries, 0.9925338310779281) // Ideal value 1
   testLocalGoodnessOfFitEquals(dataSeries, 0, 1)
   testXProjectionEquals(dataSeries, 1, -3) // Theoretical noisefree value -3
-  testLocalGoodnessOfFitEquals(dataSeries, 1, 0.9645892351274787)
+  testLocalGoodnessOfFitEquals(dataSeries, 1, 0.9813345776948204)
   testXProjectionEquals(dataSeries, 3, 3) // Theoretical noisefree value 3
   testLocalGoodnessOfFitEquals(dataSeries, 2, 0.9645892351274787)
   testXProjectionEquals(dataSeries, 4, 6) // Theoretical noisefree value 6
