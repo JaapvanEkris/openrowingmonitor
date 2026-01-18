994a36f251247d10cd04a77456ebdf883e2f7d77
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2026-01-18 22:59:32 +0100

    Adaptation to the improved startup behaviour of the CEC filter

--- app/engine/RowingStatistics.test.js
+++ app/engine/RowingStatistics.test.js
@@ -504,7 +504,7 @@ test('A full session for a Concept2 RowErg should produce plausible results', as
   await replayRowingSession(rowingStatistics.handleRotationImpulse, { filename: 'recordings/Concept2_RowErg_Session_2000meters.csv', realtime: false, loop: false })
 
   testTotalMovingTime(rowingStatistics, 590.0231672202852)
-  testTotalLinearDistance(rowingStatistics, 2027.8404221844912)
+  testTotalLinearDistance(rowingStatistics, 2027.8388877679706)
   testTotalNumberOfStrokes(rowingStatistics, 205)
   // As dragFactor isn't static, it should have changed
   testDragFactor(rowingStatistics, 80.70871681343775)
