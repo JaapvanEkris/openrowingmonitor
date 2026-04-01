9de8b44596002587ab70af44fa0ad31dd05687c5
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2026-04-01 16:20:06 +0200

    Added Chromium as preferred kiosk

--- install/webbrowserkiosk.sh
+++ install/webbrowserkiosk.sh
@@ -2,7 +2,7 @@
 #
 #  Open Rowing Monitor, https://github.com/JaapvanEkris/openrowingmonitor
 #
-#  Runs the Web Frontend in a Firefox browser in fullscreen kiosk mode
+#  Runs the Web Frontend in a Chromium or Firefox browser in fullscreen kiosk mode
 #
 xset s off
 xset s noblank
@@ -10,4 +10,10 @@ xset -dpms
 openbox-session &
 
 # Start Firefox in kiosk mode
-nice -n 5 firefox --display=:0 --kiosk-monitor 0 --kiosk http://127.0.0.1/?mode=kiosk
+if which chromium-browser >/dev/null; then
+  # chromium is installed, as we tuned this to its absolute minimum, we'll use that
+  nice -n 5 chromium-browser --kiosk --noerrdialogs --no-memcheck --disable-infobars --disable-features=AudioServiceSandbox --disable-features=TranslateUI --disable-session-crashed-bubble --no-sandbox --disable-notifications --disable-sync-preferences --no-sandbox --disable-background-mode --disable-popup-blocking --no-first-run --enable-gpu-rasterization --disable-translate --disable-logging --disable-default-apps --disable-extensions --disable-crash-reporter --disable-pdf-extension --disable-new-tab-first-run --disable-dev-shm-usage --start-maximized --mute-audio --disable-crashpad --hide-scrollbars --ash-hide-cursor --memory-pressure-off --force-device-scale-factor=1 --ignore-certificate-errors --disable-pinch --enable-low-end-device-mode --disable-site-isolation-trials --renderer-process-limit=2 --check-for-update-interval=604800 --app="http://127.0.0.1/?mode=kiosk"
+else
+  # Use firefox if Chromium wasn't installed (legacy setup for 0.9.6 installs)
+  nice -n 5 firefox --display=:0 --kiosk-monitor 0 --kiosk http://127.0.0.1/?mode=kiosk
+fi
