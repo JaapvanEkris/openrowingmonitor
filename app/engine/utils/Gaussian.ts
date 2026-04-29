d8a44f23d96532e71c5e08483fe7098218283b9e
Jaap van Ekris <82339657+JaapvanEkris@users.noreply.github.com>
2026-04-29 13:28:01 +0200

    Migration to TypeScript

--- app/engine/utils/Gaussian.ts
+++ app/engine/utils/Gaussian.ts
@@ -1,3 +1,4 @@
+'use strict'
 /**
  * @copyright {@link https://github.com/JaapvanEkris/openrowingmonitor|OpenRowingMonitor}
  *
@@ -6,27 +7,52 @@
  * Please realize the constant factor 1/Math.Pow(2 * Math.pi(), 0.5) is omitted as it cancels out in the subsequent weight averaging filtering
  */
 
-interface GaussianWeightFunction {
-  setWindowWidth: (beginpos: number, endpos: number) => void
-  weight: (position: number) => number
+// ---------------------------------------------------------------------------
+// Public interface
+// ---------------------------------------------------------------------------
+
+export interface GaussianWeight {
+  setWindowWidth(xBegin: Readonly<number>, xEnd: Readonly<number>): void
+  weight(x: Readonly<number>): number | null
 }
 
-export function createGaussianWeightFunction (): GaussianWeightFunction {
+// ---------------------------------------------------------------------------
+// Implementation
+// ---------------------------------------------------------------------------
+
+export function createGaussianWeightFunction (): GaussianWeight {
   let begin: number = 0
   let end: number = 0
   let halfLength: number = 0
   let middle: number = 0
 
-  function setWindowWidth (beginpos: number, endpos: number): void {
-    begin = beginpos
-    end = endpos
-    halfLength = (end - begin) / 2
-    middle = halfLength + begin
+  /**
+   * @description This sets the window for the Gaussian weight calculation
+   * @param {float} xBbegin - the minimum x value of the window
+   * @param {float} xEnd - the maximum x value of the window
+   */
+  function setWindowWidth (xBegin: Readonly<number>, xEnd: Readonly<number>): void {
+    if (xBegin < xEnd) {
+      begin = xBegin
+      end = xEnd
+      halfLength = (end - begin) / 2
+      middle = halfLength + begin
+    } else {
+      begin = undefined
+      end = undefined
+      halfLength = undefined
+      middle = undefined
+    }
   }
 
-  function weight (position: number): number {
-    if (position >= begin && end >= position) {
-      const normalizedDistance: number = Math.abs((middle - position) / halfLength)
+  /**
+   * @param {float} x - the x value of the datapoint
+   * @returns The calculated Gaussian weight
+   */
+  function weight (x: Readonly<number>): number {
+    if (!halfLength > 0) { return undefined }
+    if (x >= begin && end >= x) {
+      const normalizedDistance: number = Math.abs((middle - x) / halfLength)
       return Math.exp(-0.5 * Math.pow(normalizedDistance, 2))
     } else {
       return 0
